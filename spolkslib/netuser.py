4ad8386138093960a4d6b9eb8f87884acd52e1ad
root <root@ogvnrvo-Inspiron-N5010.(none)>
2013-12-02 11:36:13 +0300

    udp support added

--- spolkslib/netuser.py
+++ spolkslib/netuser.py
@@ -5,10 +5,10 @@ import sys
 def run_client(host, port, action, a_args):
 	BUF_SIZE = 65353
 	try:
-		client_socket = socket.socket(socket.AF_INET, 
+		client_socket = socket.socket(socket.AF_INET,\
 			socket.SOCK_STREAM)
-		
-		client_socket.connect((host, port))		
+
+		client_socket.connect((host, port))
 		action(client_socket, a_args)
 
 
@@ -18,7 +18,9 @@ def run_client(host, port, action, a_args):
 		print ("socket error %s" % e)
 	except IOError as e:
 		print "cannot open the file"
+	except Exception as e:
+		print e
 	finally:
 		if client_socket != None:
 			client_socket.close()
-			sys.exit(0)		
+			sys.exit(0)
