c64c2fa3db204518613340193df212eebe841db6
root <root@ogvnrvo-Inspiron-N5010.(none)>
2013-12-02 10:47:49 +0300

    filework utils added

---
+++ spolkslib/filework.py
@@ -0,0 +1,13 @@
+import os
+
+def get_fsize(f):
+	try:
+		old_file_position = f.tell()
+		f.seek(0, os.SEEK_END)
+        	size = f.tell()
+        	f.seek(old_file_position, os.SEEK_SET)
+        except Exception as e:
+		print ("exception occured %s" % e)
+	else:	
+		return size
+	
