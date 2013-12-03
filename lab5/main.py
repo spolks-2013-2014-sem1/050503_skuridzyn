0d75921f9be63dccaf3f1c3bf18c16cae853a460
root <root@ogvnrvo-Inspiron-N5010.(none)>
2013-12-03 10:55:09 +0300

    safe data transaction

--- lab5/main.py
+++ lab5/main.py
@@ -17,48 +17,54 @@ BUF_SIZE = 32*1024
 f_name = "file_1"
 
 def server_routine(conn):
+
 	global f_name
-	f = open(f_name, 'wb')
-	
-	rtr, rtw, ie = select.select([conn], [], [], 10.0)
-	if len(rtr) > 0:
+	rtr, rtw, ie = select.select([conn], [], [])
+	if conn in rtr > 0:
 		data, addr = rtwork.recieve_from(conn, BUF_SIZE)
+		f = open(f_name, 'wb')
+		print "connected by", addr
 		f.write(data)
+		rtwork.transmit_to(conn, "ACK", addr)
 	else: return True
+	
+	
 	while True:
-		rtr, rtw, ie = select.select([conn], [], [], 1.0)
-		if len(rtr) > 0:
+		rtr, rtw, ie = select.select([conn], [], [], 10.0)
+		if conn in rtr:
 			data, addr = rtwork.recieve_from(conn, BUF_SIZE)
+			if data == "FIN":
+				break
 			f.write(data)
+			rtwork.transmit_to(conn, "ACK", addr)
 		else: break
 	
-	print 'Client disconnected\nWaiting for the next...'	
-	
+	print 'Client disconnected\nWaiting for the next client...'
 	f.close()
 	f_name = f_name[:-1] + str(int(f_name[-1]) + 1)
-	return False
+	return True 
 
 
 def client_routine(s, f_name, host, port):
-	print (host, port)
+	print 'connected to', (host, port)
 	f = open(f_name, 'rb')  
    	f_size = filework.get_fsize(f)
         bytes_send = 0
                 
   	while True:
         	buffer = f.read(BUF_SIZE)
-		s.sendto(buffer, (host, port))
-           	#success = rtwork.transmit_to(s, buffer, (host, port))
-		time.sleep(0.002)
+		rtwork.transmit_to(s, buffer, (host, port))
+		rtr, rtw, ie = select.select([s], [], [], 10.0)
+		if s in rtr:
+			ack, addr = rtwork.recieve_from(s, 3)
+		if ack != "ACK": 
+			rtwork.transmit_to(s, buffer, (host, port))			
 		if bytes_send == f_size:
+			rtwork.transmit_to(s, "FIN", (host, port))
             		break
-         	#if not success:
-              	#	break
          	bytes_send += len(buffer) 
-                
     	s.close()
 	f.close()
-
 
 def main():
 	parser = argparse.ArgumentParser()
