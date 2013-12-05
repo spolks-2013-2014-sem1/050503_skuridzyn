2e2ad8f9582b2d93ffaf8840d2d351542c91aea1
root <root@ogvnrvo-Inspiron-N5010.(none)>
2013-12-05 03:20:55 +0300

    pep8 fixes

--- lab5/main.py
+++ lab5/main.py
@@ -5,65 +5,59 @@ import argparse
 import sys
 
 if __name__ == '__main__':
-        sys.path.insert(0, \
-        	os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    sys.path.insert(0, \
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from spolkslib import netserver, netparser, netclient, rtwork, filework 
-
-#from udp_client import * 
-#from tcp_client import *
-#from tcp_client_urg import *
+from spolkslib import netserver, netparser, netclient
 from server import *
 from client import *
 
+
 def main():
 
-	parser = argparse.ArgumentParser()
-	port = netparser.parse_type("port")
-	parser = argparse.ArgumentParser()
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('-t', '--tcp', action='store_true')
-	group.add_argument('-u', '--udp', action='store_true')
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('-s', '--server', type=port, dest='port')
-	group.add_argument('-c', '--client', nargs=3,  
-		type=''.join, dest='args')
-	parser.add_argument('-v', '--verbosity', action='store_true')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    port = netparser.parse_type("port")
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-t', '--tcp', action='store_true')
+    group.add_argument('-u', '--udp', action='store_true')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-s', '--server', type=port, dest='port')
+    group.add_argument('-c', '--client', nargs=3, type=''.join, dest='args')
+    parser.add_argument('-v', '--verbosity', action='store_true')
 
-	if args.args:
-		cl_nfo = netparser.parse_list(args.args)
-		cl_args = (getattr(cl_nfo, "filename"),
-				getattr(cl_nfo, "host"), 
-				getattr(cl_nfo, "port"))
-			
+    args = parser.parse_args()
 
-	if args.udp:
-		if args.port:
-			print "udp server runs on port", args.port
-			netserver.run_udp_server(args.port, udp_server)
-		elif cl_nfo:
-			print ("connecting to (%s, %s)..." % 
-				(cl_nfo.host, cl_nfo.port))
+    if args.args:
+        cl_nfo = netparser.parse_list(args.args)
+        cl_args = (getattr(cl_nfo, "filename"), getattr(cl_nfo, "host"),
+        getattr(cl_nfo, "port"))
 
-			netclient.run_udp_client(cl_nfo.host, 
-			cl_nfo.port, udp_client, cl_args)
-	elif args.tcp:
-		if args.port:
-			print "tcp server runs on port", args.port
-			if args.verbosity:
-				netserver.run_tcp_server(args.port,
-					tcp_server_urg)
-			else: netserver.run_tcp_server(args.port, tcp_server)
-		elif cl_nfo:
-			print ("connecting to (%s, %s)" % 
-				(cl_nfo.host, cl_nfo.port))
-			if args.verbosity:
-				netclient.run_tcp_client(cl_nfo.host,
-				cl_nfo.port, tcp_client_urg, cl_nfo.filename)
-			else:
-				netclient.run_tcp_client(cl_nfo.host,
-				cl_nfo.port, tcp_client, cl_nfo.filename)
-		
+    if args.udp:
+        if args.port:
+            print "udp server runs on port", args.port
+            netserver.run_udp_server(args.port, udp_server)
+        elif cl_nfo:
+            print ("connecting to (%s, %s)..." % (cl_nfo.host, cl_nfo.port))
+            netclient.run_udp_client(cl_nfo.host, cl_nfo.port,
+            udp_client, cl_args)
+
+    elif args.tcp:
+        if args.port:
+            print "tcp server runs on port", args.port
+            if args.verbosity:
+                netserver.run_tcp_server(args.port, tcp_server_urg)
+            else:
+                netserver.run_tcp_server(args.port, tcp_server)
+        elif cl_nfo:
+            print ("connecting to (%s, %s)" % (cl_nfo.host, cl_nfo.port))
+            if args.verbosity:
+                netclient.run_tcp_client(cl_nfo.host, cl_nfo.port,
+                tcp_client_urg, cl_nfo.filename)
+            else:
+                netclient.run_tcp_client(cl_nfo.host, cl_nfo.port,
+                tcp_client, cl_nfo.filename)
+
+
 if __name__ == "__main__":
-	main()
+    main()
--- lab5/server.py
+++ lab5/server.py
@@ -1,80 +1,87 @@
-import socket, select, sys, os
+import socket
+import select
+import sys
+import os
 from spolkslib import rtwork, filework
-
-
 
 __TCP_BUF_SIZE = 65536
 __UDP_BUF_SIZE = 32768
 
+
 def __tcp_server_routine(server, verbosity=False):
 
-        data_length = 0
-	conn, addr = server.accept()
-        print "connected by", addr		
-        
-	filename = filework.random_name()
-        f = open(filename, 'wb')
+    data_length = 0
+    conn, addr = server.accept()
+    print "connected by", addr
 
-        while True:
-                rtr, rtw, ie = select.select([conn], [], [conn], 10.0)
-                
-		if conn in ie and verbosity == True:
-                        urg = conn.recv(__TCP_BUF_SIZE, socket.MSG_OOB)
-                        data = rtwork.recieve(conn, __TCP_BUF_SIZE)
-                        f.write(data)
-                        print ("%s bytes recieved" % data_length)
+    filename = filework.random_name()
+    f = open(filename, 'wb')
 
-                elif conn in rtr:
-                        data = rtwork.recieve(conn, __TCP_BUF_SIZE)
-                        if not data: break
-                        f.write(data)
-                else: break
-                data_length += len(data)
+    while True:
+        rtr, rtw, ie = select.select([conn], [], [conn], 10.0)
 
-        f.close()
-        conn.close()
-        print 'Client disconnected\nWaiting for the next client...'
-        return True
+        if conn in ie and verbosity == True:
+            urg = conn.recv(__TCP_BUF_SIZE, socket.MSG_OOB)
+            data = rtwork.recieve(conn, __TCP_BUF_SIZE)
+            f.write(data)
+            print ("%s bytes recieved" % data_length)
+
+        elif conn in rtr:
+            data = rtwork.recieve(conn, __TCP_BUF_SIZE)
+            if not data:
+                break
+            f.write(data)
+        else:
+            break
+
+        data_length += len(data)
+
+    f.close()
+    conn.close()
+    print 'Client disconnected\nWaiting for the next client...'
+
+    return True
+
 
 def __udp_server_routine(server):
 
-        rtr, rtw, ie = select.select([server], [], [])
+    rtr, rtw, ie = select.select([server], [], [])
+    if server in rtr:
+        data, client = rtwork.recieve_from(server, __UDP_BUF_SIZE)
+        filename = filework.random_name()
+        f = open(filename, 'wb')
+        print "connected by", client
+        f.write(data)
+        rtwork.transmit_to(server, "ACK", client)
+    else:
+        return True
+
+    eof_detected = False
+    while True:
+        rtr, rtw, ie = select.select([server], [], [], 10.0)
         if server in rtr:
-                data, client = rtwork.recieve_from(server, __UDP_BUF_SIZE)
-		filename = filework.random_name()
-                f = open(filename, 'wb')
-                print "connected by", client
-                f.write(data)
-                rtwork.transmit_to(server, "ACK", client)
-        else: return True
+            data, addr = rtwork.recieve_from(server, __UDP_BUF_SIZE)
+            if client != addr:
+                continue
+            if data == "FIN":
+                eof_detected = True
+                break
+            f.write(data)
+            rtwork.transmit_to(server, "ACK", client)
+        else:
+            break
 
-        eof_detected = False
-        while True:
-                rtr, rtw, ie = select.select([server], [], [], 10.0)
-                if server in rtr:
-                        data, addr = rtwork.recieve_from(server, __UDP_BUF_SIZE)
-                        if client != addr:
-                                continue
-                        if data == "FIN":
-                                eof_detected = True
-                                break
-                        f.write(data)
-                        rtwork.transmit_to(server, "ACK", client)
-                else: break
-
-        if not eof_detected:
-                print 'eof is not detected! erase bufer data...'
+    if not eof_detected:
+        print 'eof is not detected! erase bufer data...'
+        f.close()
+        os.remove(filename)
+    else:
         print 'Client disconnected\nWaiting for the next client...'
         f.close()
 
-        if not eof_detected:
-                os.remove(filename)
+    return True
 
-        return True
 
 tcp_server = __tcp_server_routine
-
 tcp_server_urg = lambda s: __tcp_server_routine(s, True)
-
 udp_server = __udp_server_routine
-
